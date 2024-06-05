30c01802dc74e918ed26c9b75857faa7553b8038
Yevhenii <polotnyak2004@gmail.com>
2024-06-05 14:40:11 +0300

    make build

---
+++ index.f4bad46c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.left="".concat((e.clientWidth-t.clientWidth)/2,"px"),t.style.top="".concat((e.clientHeight-t.clientHeight)/2,"px");
+//# sourceMappingURL=index.f4bad46c.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,7 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+spider.style.left = `${(wall.clientWidth - spider.clientWidth) / 2}px`;
+spider.style.top = `${(wall.clientHeight - spider.clientHeight) / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
